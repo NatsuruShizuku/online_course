7278452afc65a812d9c31b6d148b6ff8bf16cd9e
SaDyZ P120 <ponlamart_t@silpakorn.edu>
2023-08-27 15:28:47 +0700

    code1

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var value1 = false;
+  //
 
   @override
   Widget build(BuildContext context) {
@@ -16,7 +24,10 @@ class HomePage extends StatelessWidget {
                 Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Text('Hello,', style: TextStyle(color: Colors.grey[500]),),
+                    Text(
+                      'Hello,',
+                      style: TextStyle(color: Colors.grey[500]),
+                    ),
                     Row(
                       children: [
                         Padding(
@@ -26,28 +37,154 @@ class HomePage extends StatelessWidget {
                             style: TextStyle(
                               fontSize: 20.0,
                               fontWeight: FontWeight.bold,
-                              ),
+                            ),
                           ),
                         ),
-                        //Icon(Icons.back_hand, size: 40.0,),
-                        //SizedBox(width: 12.0),
-                        Image.asset('assets/images/hand.png', width: 40.0,)
+                        //SizedBox(width: 8.0),
+                        Image.asset(
+                          'assets/images/hand.png',
+                          width: 40.0,
+                        ),
                       ],
-                    ),
+                    )
                   ],
                 ),
                 SizedBox(
                   width: 45.0,
                   height: 45.0,
                   child: CircleAvatar(
-                    backgroundImage: AssetImage('assets/images/profile.jpg'), //หาภาพมาใส่
-
+                    backgroundImage: AssetImage('assets/images/profile.jpg'),
                   ),
-                ),
+                )
               ],
             ),
-          ), //ส่วน app bar
-          Container(), //ส่วนที่เหลือ
+          ),
+          Expanded(
+            child: Container(
+              color: Color.fromARGB(255, 222, 250, 255),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 24.0, 16.0, 8.0),
+                    child: Text('Courses', style: TextStyle(fontSize: 26.0)),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Row(
+                      children: [
+                        // ปุ่ม All Topics
+                        Expanded(
+                          child: _buildCategoryButton(
+                            'All Topics',
+                            Icons.water_drop,
+                            Colors.lightBlue,
+                            value1,
+                          ),
+                        ),
+                        SizedBox(width: 16.0),
+                        // ปุ่ม Popular
+                        Expanded(
+                          child: _buildCategoryButton(
+                            'Popular',
+                            Icons.bolt,
+                            Colors.pink,
+                            true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    child: Row(
+                      children: [
+                        // ปุ่ม Newest
+                        Expanded(
+                          child: _buildCategoryButton(
+                            'Newest',
+                            Icons.star,
+                            Colors.orange,
+                            true,
+                          ),
+                        ),
+                        SizedBox(width: 16.0),
+                        // ปุ่ม Advanced
+                        Expanded(
+                          child: _buildCategoryButton(
+                            'Advanced',
+                            Icons.bookmark,
+                            Colors.green,
+                            true,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Center(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ElevatedButton(
+                        onPressed: () {
+                          setState(() {
+                            value1 = !value1;
+                          });
+                        },
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Text('TEST'),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Container _buildCategoryButton(
+      String text, IconData icon, Color color, bool isSelected) {
+    return Container(
+      decoration: BoxDecoration(
+        color: isSelected ? color : Colors.white,
+        borderRadius: BorderRadius.circular(50.0),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 7,
+            blurRadius: 12,
+            offset: Offset(0, 10), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            margin: const EdgeInsets.all(12.0),
+            decoration: BoxDecoration(
+              color: isSelected ? Colors.white : color,
+              shape: BoxShape.circle,
+            ),
+            child: Icon(
+              icon,
+              color: isSelected ? color : Colors.white,
+            ),
+          ),
+          Text(
+            text,
+            style: TextStyle(color: isSelected ? Colors.white : Colors.black87),
+          ),
         ],
       ),
     );
